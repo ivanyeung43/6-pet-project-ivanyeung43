8422475172b801875ea5f3ba0827eda0b09add86
ivanyeung43 <ivany43@nycstudents.net>
2026-04-21 09:25:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,17 @@ John.buy({"title:": "Chestplate", "def": 10, "price": 12})
 print(John.__dict__) """
 
 class Pet:
-    def __init__(self, name, happiness):
+    def __init__(self, name, happiness, action):
         self.name = name
         self.__happiness = happiness
-    def play(self, fetch):
+        self.action = ""
+    def play(self, action):
         self.__happiness += 10
+        self.action = ""
+        print(f"{self.name} is playing{self.action}")
     def show_status(self):
-        print(f"{self.name}")
+        print(f"{self.name} has {self.__happiness}")
+
+Ruffs = Pet("Ruffs", 1, "")
+Ruffs.play("Catch")
+print(Ruffs.__dict__)

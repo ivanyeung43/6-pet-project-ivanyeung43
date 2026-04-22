243745b6b386c0d6d4ba55fede25bd4706bd34ad
ivanyeung43 <ivany43@nycstudents.net>
2026-04-22 09:24:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Lesson 1
 """ class Hero:
     def __init__(self, name, money,  inventory):
         self.name = name
@@ -11,18 +12,33 @@ John = Hero("John", 10, ["Map"])
 John.buy({"title:": "Chestplate", "def": 10, "price": 12})
 print(John.__dict__) """
 
-class Pet:
+""" class Pet:
     def __init__(self, name, happiness, action):
         self.name = name
         self.__happiness = happiness
-        self.action = ""
+        self.action = action
     def play(self, action):
         self.__happiness += 10
-        self.action = ""
+        self.action = action
         print(f"{self.name} is playing{self.action}")
     def show_status(self):
-        print(f"{self.name} has {self.__happiness}")
+        print(f"{self.name} has {self.__happiness} happiness.")
 
 Ruffs = Pet("Ruffs", 1, "")
-Ruffs.play("Catch")
-print(Ruffs.__dict__)
+Ruffs.play(" fetch.")
+Ruffs.show_status() """
+
+# Pet Project
+
+class Pet:
+    def __init__(self, name, happiness, health, action, hunger):
+        self.name = name
+        self.__happiness = happiness
+        self.__health = health
+        self.action = action
+        self.hunger = hunger
+    def Naming(self, name):
+        self.name = input(name)
+        print(input("What would you like to name the pet"))
+
+"" = Pet
